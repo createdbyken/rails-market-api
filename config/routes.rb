abf41fba783e5a1ba11c0cd915de308318a7d04a
Ken Sanchez <kensanchez16@gmail.com>
2021-08-21 19:38:29 -0500

    versioning rails routes api

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
- #Api definition
- namespace :api, defaults:{format: json} do
-
- end
+  # Api definition
+  namespace :api, defaults: { format: json } do
+    namespace :v1 do
+    end
+  end
 end
